8f20187db688a755aa467c0b25b2b26628c7d1db
Mahdi Hassan <mahdioffi333@gmail.com>
2025-07-14 01:48:37 +0600

    Create Custom Error Class, AppErro

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from "express"
 import { envVars } from "../config/env"
-// import AppError from "../errorHelpers/AppError"
+import AppError from "../errorHelpers/AppError"
 
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
 
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,10 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from 'http-status-codes';
 import { UserServices } from "./user.service";
+import AppError from "../../errorHelpers/AppError";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        
+        // throw new Error('Fake error')
+        throw new AppError(httpStatus.BAD_REQUEST, 'fake error')
         const user = await UserServices.createUser(req.body)
         
         res.status(httpStatus.CREATED).json({
