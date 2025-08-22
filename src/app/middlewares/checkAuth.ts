6802ae19b800edd2789fb9aab6b16c85367c09b9
Mahdi Hassan <mahdioffi333@gmail.com>
2025-08-22 18:13:43 +0600

    Introduction to Redis, Setting and Configuring Redis

--- src/app/middlewares/checkAuth.ts
+++ src/app/middlewares/checkAuth.ts
@@ -10,10 +10,10 @@ import { verifyToken } from "../utils/jwt";
 export const checkAuth = (...authRoles: string[]) => async (req: Request, res: Response, next: NextFunction) => {
 
     try {
-        const accessToken = req.headers.authorization;
+        const accessToken = req.headers.authorization || req.cookies.accessToken;
 
         if (!accessToken) {
-            throw new AppError(403, "No Token Recieved")
+            throw new AppError(403, "No Token Received")
         }
 
 
